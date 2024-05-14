7f49f8b29673abc4baaba7dfc9633ad423ad128e
archae13 <archae13@lpbroadband,net>
2024-05-14 14:12:02 -0600

    added 3 squares

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,18 @@
+const Square = () => {
+  return <button></button>;
+};
+
 const Board = () => {
   const [player, setPlayer] = React.useState(0);
   let status = `Player ${player}`;
+  function renderSquare() {
+    return <Square></Square>;
+  }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer(player + 1);
-        status = `Player ${player}`;
-      }}
-    >
+    <div className="game-board">
+      <div className="grid-row">
+        {renderSquare(0)} {renderSquare(1)} {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
